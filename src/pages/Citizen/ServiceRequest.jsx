305c64fbad1a3b70d81eb056aed78b3b3495856f
Camila Braz <camilabvieira74@gmail.com>
2024-11-02 16:53:55 -0300

    add: adiciona alert e info sobre local de onde o serviço irá ser solicitado, e info sobre adição de imagem

--- src/pages/Citizen/ServiceRequest.jsx
+++ src/pages/Citizen/ServiceRequest.jsx
@@ -74,7 +74,9 @@ const ServiceAdd = () => {
 
 
   return (
+
     <BaseForm onSubmit={handleFormSubmit}>
+
       <div>
         <label htmlFor="servico" className="block text-sm font-medium text-gray-700">Serviço</label>
         <input
@@ -112,6 +114,21 @@ const ServiceAdd = () => {
           className="input input-bordered w-full mt-1"
           required
         />
+      </div>
+
+      <div role="alert" className="alert alert-warning">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+        <span>Atenção! O endereço da solicitação deve ser o do local onde o serviço será realizado.</span>
       </div>
 
       <div>
@@ -205,6 +222,22 @@ const ServiceAdd = () => {
           className="input input-bordered w-full mt-1"
           required
         />
+      </div>
+
+      <div role="alert" className="alert">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-info h-6 w-6 shrink-0">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
+        <span>Adicionar foto facilita o atendimento no local do serviço solicitado.</span>
+
       </div>
 
       <div>
