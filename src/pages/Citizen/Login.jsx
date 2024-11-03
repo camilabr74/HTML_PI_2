28de8496a289ddb639bfc03e83b6ce182ee635f0
Digo-dev <dev.ctbn@gmail.com>
2024-11-02 22:11:55 -0300

    Iniciada a criação da tela de login do usuário comum, mesmo codigo da login-employee

--- src/pages/Citizen/Login.jsx
+++ src/pages/Citizen/Login.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BaseForm from '../../components/BaseForm/BaseForm';
 
-function Login() {
+
+  const Login = ({ onSubmit }) => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleFormSubmit = () => {
+    onSubmit({ email, senha });
+    setEmail('');
+    setSenha('');
+  };
+
   return (
-    <div>
-      <h1>Welcome to My Simple Page!</h1>
-      <p>This is a basic example of a React application.</p>
-    </div>
+    <BaseForm onSubmit={handleFormSubmit}>
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+          E-mail
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite seu e-mail"
+          className="input input-bordered w-full mt-1"
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="senha" className="block text-sm font-medium text-gray-700">
+          Senha
+        </label>
+        <input
+          type="senha"
+          id="senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Digite sua senha"
+          className="input input-bordered w-full mt-1"
+          required
+        />
+      </div>
+    </BaseForm>
   );
-}
+};
 
 export default Login;
