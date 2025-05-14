3154d43ddf5b5f8a527e1c41c750398c51ddaad6
Camila <camilabvieira74@gmail.com>
2025-05-14 16:02:38 -0300

    feat: adiciona botão para visualizar os dados

--- src/pages/Goverment/HomeEmployee.jsx
+++ src/pages/Goverment/HomeEmployee.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ServiceList from '../../components/ServiceList/ServiceList';
+import ButtonCTA from "../../components/ButtonCTA/ButtonCTA";
+
 
 function HomeEmployee() {
   return (
@@ -8,6 +10,10 @@ function HomeEmployee() {
       <h1 className="text-2xl p-4 font-bold text-accent">
         Bem-vindo ao Portal, servidor!
       </h1>
+
+      <section className="w-full flex justify-center py-8">
+        <ButtonCTA to="/service-selection">Visualizar Dados</ButtonCTA>
+      </section>
 
       <ServiceList endpoint='https://orlok.pythonanywhere.com/api/v1/janitorial/' />
 
