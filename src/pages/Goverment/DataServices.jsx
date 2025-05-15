2f7ec3576ba6aef45900adcac7f2a8b252e342bb
camilabr74 <camilabvieira74@gmail.com>
2025-05-15 09:52:13 -0300

    add: cria página para análise de dados

---
+++ src/pages/Goverment/DataServices.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ServiceList from '../../components/ServiceList/ServiceList';
+import ButtonCTA from "../../components/ButtonCTA/ButtonCTA";
+
+function DataService() {
+  return (
+    <div>
+
+      <h1 className="text-2xl p-4 font-bold text-accent">
+        Análise de Dados: Serviços de Zeladoria de São Vicente
+      </h1>
+
+
+    </div>
+
+  );
+}
+
+export default DataService;
